f3d4fc7bbdb0633e43254e956abffc4454aad581
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2020-01-13 21:11:57 +0600

    Вариант Галыма

--- HttpWebServer/Program.cs
+++ HttpWebServer/Program.cs
@@ -1,35 +1,47 @@
 ﻿using System;
 using System.IO;
 using System.Net;
-using System.Net.Http;
 
-namespace HttpWebServer
+namespace HttpWebServerProject
 {
     class Program
     {
-        // https://metanit.com/sharp/net/7.1.php
         static void Main(string[] args)
         {
             using (var listener = new HttpListener())
             {
                 listener.Prefixes.Add("http://localhost/");
                 listener.Start();
-                // Получить информацию о входящем соединении
-                var context = listener.GetContext();
+                while (true)
+                {
+                    var context = listener.GetContext();
+                    var request = context.Request;
 
-                // Получить сам запрос, можно его проанализировать
-                var request = context.Request;
+                    var searchingFileUri = string.Empty;
+                    if (request.RawUrl.IndexOf('?') != -1)
+                        searchingFileUri = request.RawUrl.Substring(0, request.RawUrl.IndexOf('?'));
+                    else
+                        searchingFileUri = request.RawUrl;
 
-                var searchingFileUri = request.RawUrl.Remove('/');
 
-                var response = context.Response;
 
-                // Не только текст, фото, видео, т.е. массив байтов
-                var responseText = File.ReadAllText(searchingFileUri);
-                var bytes = System.Text.Encoding.UTF8.GetBytes(responseText);
-                response.OutputStream.Write(bytes, 0, bytes.Length);
-                response.Close();
+                    var response = context.Response;
+                    if (File.Exists(@"C:\Users\нэт\source\repos\HttpWebServerProject\HttpWebServerProject\bin\Debug\netcoreapp3.0" + searchingFileUri))
+                    {
+                        var responseText = File.ReadAllText(@"C:\Users\нэт\source\repos\HttpWebServerProject\HttpWebServerProject\bin\Debug\netcoreapp3.0" + searchingFileUri);
+                        var bytes = System.Text.Encoding.UTF8.GetBytes(responseText);
+                        response.OutputStream.Write(bytes, 0, bytes.Length);
+                    }
+                    else
+                    {
+                        var bytes = new byte[0];
+                        response.OutputStream.Write(bytes, 0, bytes.Length);
+                    }
+                    response.Close();
+                }
             }
+
+
         }
     }
 }
