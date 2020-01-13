7ce4b9b8cdd842e2c22275aa745f4173542567a0
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2020-01-13 19:53:21 +0600

    Добавьте файлы проекта.

---
+++ HttpWebServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace HttpWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var listener = new HttpListener())
+            {
+
+            }
+        }
+    }
+}
